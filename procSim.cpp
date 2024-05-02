f9bded0036b0f92575543137c16bd8b03e8c54f2
Kaevin <135804688+KDawg2004@users.noreply.github.com>
2024-05-01 17:20:43 -0700

    Create procSim.cpp

---
+++ procSim.cpp
@@ -0,0 +1,17 @@
+  1 /**
+  2 *  Process Queue Simulation file (not implemented)
+  3 *  Used in makefile assignment
+  4 */
+  5
+  6 #include <iostream>
+  7 #include "priQueueInt.h"
+  8
+  9 int main() {
+ 10     PriQueueInt q;
+ 11
+ 12     q.insert(42,3);
+ 13
+ 14     std::cout << "sim ran\n";
+ 15
+ 16     return 0;
+ 17 }
